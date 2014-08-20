f855fe49dd1a79641e202b7a5fb39ce77dfeeaf6
dmiramon <dmiramon@gmail.com>
2014-08-19 19:43:27 -0500

    Prueba 2 de daemon Diego

--- script/daemon.rb
+++ script/daemon.rb
@@ -2,9 +2,4 @@
 require 'rubygems'
 require 'daemons'
 
-ENV["APP_ROOT"] ||= File.expand_path("#{File.dirname(__FILE__)}/..")
-ENV["RAILS_ENV_PATH"] ||= "#{ENV["APP_ROOT"]}/config/environment.rb"
-
-script = "#{ENV["APP_ROOT"]}/lib/daemons/#{ARGV[1]}"
-
-Daemons.run(script, dir_mode: :normal, dir: "#{ENV["APP_ROOT"]}/tmp/pids") 
+Daemons.run('twitter_stream.rb') 
--- script/twitter_stream.rb
+++ script/twitter_stream.rb
@@ -1,4 +1,9 @@
-require ENV["RAILS_ENV_PATH"]
+#!/usr/bin/env ruby
+ENV["RAILS_ENV"] = "development"
+
+#root = File.expand_path(File.join(File.dirname(__FILE__), '../..'))
+#require File.join(root, "config", "environments", ENV["RAILS_ENV"])
+require File.dirname(__FILE__) + "/../config/environment"
 
 client = Twitter::Streaming::Client.new do |config|
   config.consumer_key    = ENV['CONSUMER_TOKEN']
