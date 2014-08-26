14087910ca2f26a26fa01576fac307725674a3a1
dmiramon <dmiramon@gmail.com>
2014-08-26 00:53:10 -0500

    Integración en tiempo real con Instagram
    Diego

--- app/controllers/display_controller.rb
+++ app/controllers/display_controller.rb
@@ -21,31 +21,15 @@ class DisplayController < ApplicationController
       return
   	else
   	  #Instagram.process_subscription(params[:body]) do |handler|
-        puts '------------------------'
+        puts '------------------------diego'
         #puts handler.to_yaml
         #Thread.new do
           fetch_new_photos
         #end
-        puts '------------------------'
+        puts 'diego------------------------'
   	  #end
   	end
     render plain: 'ok'
-  end
-
-  def test_twitter
-    client = Twitter::Streaming::Client.new do |config|
-      config.consumer_key    = ENV['CONSUMER_TOKEN']
-      config.consumer_secret = ENV['CONSUMER_SECRET']
-      config.access_token    = ENV['ACCESS_TOKEN']
-      config.access_token_secret   = ENV['ACCESS_SECRET']
-    end
-
-    test = {:name => 'Diego', :time => Time.now.to_i}
-    WebsocketRails[:twitter_channel].trigger(:new_tweet, test.to_json)
-    #client.filter(:track => 'foodporn') do |object|
-    #  puts object.text if object.is_a?(Twitter::Tweet)
-    #end
-		render plain: 'ok'
   end
 
   def fetch_new_photos
@@ -54,14 +38,12 @@ class DisplayController < ApplicationController
     next_min_id = util.next_min_id
     results = instagram.tag_recent_media('gdlestradicional', {:min_tag_id => next_min_id})
     if results.size > 0
-      util.update_attribute(:next_min_id, results.pagination.min_tag_id)
       results.each do |object|
-        puts '---------------'
-        puts object.id
-        puts object.caption
-        puts '---------------'
-        Photo.create(instagram_id: object.id, caption: object.caption.text, author_id: object.user.id, author_nickname: object.user.username, lat: object.location.latitude, long: object.location.longitude, url_low: object.images.low_resolution.url, url_thumb: object.images.thumbnail.url, url_normal: object.images.standard_resolution.url, points: 2)
+        if object.location
+          Photo.create(instagram_id: object.id, caption: object.caption.text, author_id: object.user.id, author_nickname: object.user.username, lat: object.location.latitude, long: object.location.longitude, url_low: object.images.low_resolution.url, url_thumb: object.images.thumbnail.url, url_normal: object.images.standard_resolution.url, points: 2)
+        end  
       end
+      util.update_attribute(:next_min_id, results.pagination.min_tag_id)
     end
   end
 
