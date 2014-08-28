6f1aaeec3f79b7d0ef70a8a95770f8796a53b245
tonklis <benjamin.hernandez@in2teck.com>
2014-08-28 12:56:35 -0500

    Administrador afinado con usuarios #15675732177961

--- app/admin/activeadmin_user.rb
+++ app/admin/activeadmin_user.rb
@@ -1,16 +1,56 @@
 ActiveAdmin.register User do
 
+	actions :all, :except => [:new, :edit, :destroy] 
+
   permit_params :active
+
+	controller do
+		def update
+	    update! do |format|
+  	    format.html { redirect_to admin_users_path }
+    	end
+	  end
+  end
+
+	index do
+		column :uid
+		column :name
+		column :nickname
+		column :email
+		column :active
+		column "" do |user|
+				link_to "View", admin_user_path(user)
+		end
+		#column "" do |user|
+		#		link_to "Edit", edit_admin_user_path(user)
+		#end
+	end
+
+	form do |f|
+		f.inputs "User details" do
+			f.input :uid, :as => :string, :input_html => { :disabled => true, :style => "background-color: #d3d3d3;" }
+			f.input :name, :as => :string, :input_html => { :disabled => true, :style => "background-color: #d3d3d3;" }
+			f.input :nickname, :as => :string, :input_html => { :disabled => true, :style => "background-color: #d3d3d3;" }
+			f.input :email, :as => :string, :input_html => { :disabled => true, :style => "background-color: #d3d3d3;" }
+			f.input :active
+		end
+		f.actions
+	end
 
 	show do |user|
 		attributes_table do
     	row :uid
     	row :nickname
-			table_for user.photos do
-				column "Photo", :url_thumb do |photo|
+			table_for user.photos do 
+				column "Photo", :url_thumb, :class => "photo_alignment" do |photo|
 					link_to( (image_tag photo.url_thumb), photo.url_normal, :target => "_blank" )
 				end
-				column "", :editar do |photo|
+				column :location, :class => "photo_alignment"
+				column :caption, :class => "photo_alignment"
+				column :points, :class => "photo_alignment"
+				column :active, :class => "photo_alignment"
+				column :created_at, :class => "photo_alignment"
+				column "", :editar, :class => "photo_alignment" do |photo|
 					link_to( "Editar", edit_admin_photo_path(photo) )
 				end
 			end
